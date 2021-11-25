66df53a67d7319b2196508831ac29aef0defd1c2
Azubuike Njoku <76952451+DrZubby@users.noreply.github.com>
2021-11-25 17:20:26 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). 
+
+This tip calculator is able to perform calculations for any amount of tip you intend to donate and for any number of people based on a percentage from your bill.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties*
+- Flexbox
+- Mobile-first workflow
+- Vanilla JavaScript
+
+*HTML and JS almost fully implemented while CSS styling is next on my agenda. 
+
+### Continued development
+
+A simple project to test how much grasp I have on vanilla JS concepts while I continue my journey learning React. Speaking of React, I intend to produce a React version of this App in the nearest future!
+Expect an edit soon!
+
---
+++ script.js
@@ -0,0 +1,96 @@
+const bill = document.querySelector("#bill-input");
+
+const fiveBtn = document.querySelector("#five");
+const tenBtn = document.querySelector("#ten");
+const fifteenBtn = document.querySelector("#fifteen");
+const twentyFiveBtn = document.querySelector("#twenty-five");
+const fiftyBtn = document.querySelector("#fifty");
+const custom = document.querySelector("#custom");
+
+let totalValue = document.querySelector("#total-value");
+let tipAmount = document.querySelector("#tip-amount-value");
+
+const numOfPeople = document.querySelector("#num-of-people-input");
+
+const resetBtn = document.querySelector("#reset");
+
+function handler(btn) {
+    let content = btn.textContent;
+    let contentCustom = btn.value;
+    switch (content) {
+        case "5%":
+            return 5
+
+        case "10%":
+            return 10;
+
+        case "15%":
+            return 15
+
+        case "25%":
+            return 25
+
+        case "50%":
+            return 50
+
+        default:
+            return +contentCustom;
+    }
+}
+
+function handle(btn) {
+    let totalPeople = +numOfPeople.value;
+    if(totalPeople) {
+        let handlerOutput = handler(btn);
+        let billValue = +bill.value;
+        console.log(handlerOutput)
+        let calculated = handlerOutput / 100 * billValue;
+        totalValue.textContent = "$" +calculated;
+        tipAmount.textContent = "$" + calculated / totalPeople;
+    } else if (totalPeople === 0) {
+        tipAmount.textContent = "Can't be 0"
+    } else  tipAmount.textContent = "Can't be empty"
+}
+
+fiveBtn.onclick = (e) => {
+    e.preventDefault();
+    handle(fiveBtn)
+}
+
+tenBtn.onclick = (e) => {
+    e.preventDefault();
+    handle(tenBtn)
+}
+
+fifteenBtn.onclick = (e) => {
+    e.preventDefault();
+    handle(fifteenBtn)
+}
+
+twentyFiveBtn.onclick = (e) => {
+    e.preventDefault();
+    handle(twentyFiveBtn)
+}
+
+fiftyBtn.onclick = (e) => {
+    e.preventDefault();
+    handle(fiftyBtn)
+}
+
+custom.onkeypress = (e)=> {
+    if (e.which === 13) {
+        e.preventDefault();
+        console.log("here")
+        handle(custom)
+    }
+}
+
+resetBtn.onclick = ()=> {
+    bill.value = "";
+    totalValue.textContent = "$0.00";
+    tipAmount.textContent =  "$0.00";
+    totalPeople = "0";
+    custom.value = "";
+    numOfPeople.value = "";
+    bill.focus();
+}
